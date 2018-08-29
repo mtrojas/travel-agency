4f2a17c4be1fb3365788771e4cbfffd46d47af37
mtrojas <tererojas.i@gmail.com>
2018-08-29 13:16:44 -0500

    Make the reveal on scroll functionality recyclable and add it to testimonial section with a different offset value

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,8 +2,9 @@ import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
-  constructor() {
-    this.itemsToReveal = $('.feature-item');
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.offsetPercentage = offset;
     this.hideInitially();
     this.createWaypoints();
   }
@@ -11,6 +12,7 @@ class RevealOnScroll {
     this.itemsToReveal.addClass('reveal-item');
   }
   createWaypoints() {
+    var that = this;
     this.itemsToReveal.each(function() {
       var currentItem = this;
       new Waypoint({
@@ -18,7 +20,7 @@ class RevealOnScroll {
         handler: function() {
           $(currentItem).addClass('reveal-item--is-visible');
         },
-        offset: '85%'
+        offset: that.offsetPercentage
       });
     });
   }
